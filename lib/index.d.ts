47262815d667bc94ece7a595900acdd7f54b2120
LiuToki <ray.usaneko@gmail.com>
2021-08-08 16:28:34 +0900

    [load-visa-file] 不要なコードの削除

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,4 @@
-import { VISA_AUX } from "./load-visa-file-aux";
 export declare namespace GASLibLoadVisaFile {
-    class VISA extends VISA_AUX {
-    }
     /**
      * Setup for menu bar, please calling onOpne.
      * @param menuName Menu name for menu bar.
@@ -18,6 +15,7 @@ export declare namespace GASLibLoadVisaFile {
      * @param colRemarks Insert remarks column.
      * @param colCard Insert "カード" column.
      * @param colOne Insert "1" column.
+     * @description Each column number in the range 0 to colNum, otherwise output is empty string.
      */
     function callbackVisa(formObject: any, colNum: number, colDate: number, colName: number, colAmount: number, colRemarks: number, colCard: number, colOne: number): void;
 }
